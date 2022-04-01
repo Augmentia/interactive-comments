d3d27de39bd47bfb8dc5eda30deb25f8cd031c10
Robby Fenney <robbyfenney@fentek.com>
2022-04-01 09:48:36 -0500

    setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 class="bg-black h-8 w-8">Main Page</h1>
+    </div>
+  );
+}
+
+export default App;
